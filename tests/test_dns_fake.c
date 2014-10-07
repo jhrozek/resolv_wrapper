32bc6cff5829d48425951253862e71c489a1b981
Jakub Hrozek <jhrozek@redhat.com>
2014-10-07 10:14:51 +0200

    SOA comment fix

--- tests/test_dns_fake.c
+++ tests/test_dns_fake.c
@@ -238,8 +238,8 @@ static void test_res_fake_soa_query(void **state)
 
 	ns_initparse(answer, 256, &handle);
 	/* The query must finish w/o an error, have one answer and the answer
-	 * must be a parseable RR of type A and have the address that our
-	 * test server sends
+	 * must be a parseable RR of type SOA and have the data as in the fake
+	 * hosts file
 	 */
 	assert_int_equal(ns_msg_getflag(handle, ns_f_rcode), ns_r_noerror);
 	assert_int_equal(ns_msg_count(handle, ns_s_an), 1);
